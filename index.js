5285a0ba6ecb6be68a6756adcb6020894e68b8fa
ianderrick <ianderrick341@gmail.com>
2025-03-28 22:25:46 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function countDown(num) {   
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+
+countDown(10);
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
